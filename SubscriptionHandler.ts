b99a62f44b574c94c0567a6fe7b018ed71111cec
Chris-Bitler <chris@cbitler.io>
2022-02-10 00:49:47 -0800

    Figure out which we don't have permission for

--- SubscriptionHandler.ts
+++ SubscriptionHandler.ts
@@ -18,29 +18,30 @@ export class SubscriptionHandler {
     }
 
     attemptSubscribe(targetUserId: string) {
+        console.log("1");
         this.middlewareInstance.subscribeToChannelRaidEventsTo(targetUserId, (event) => {
             console.log(`Raid event: ${targetUserId} - number of websockets: ${this.listeners.length}`);
             this.listeners.forEach((eventHandler) => eventHandler.handleRaid(event));
         });
+        console.log("2");
         this.middlewareInstance.subscribeToChannelCheerEvents(targetUserId, (event) => {
             console.log(`Cheer event: ${targetUserId} - number of websockets: ${this.listeners.length}`);
             this.listeners.forEach((eventHandler) => eventHandler.handleCheer(event));
         });
+        console.log("3");
         this.middlewareInstance.subscribeToChannelFollowEvents(targetUserId, (event) => {
             console.log(`Follow event: ${targetUserId} - number of websockets: ${this.listeners.length}`);
             this.listeners.forEach((eventHandler) => eventHandler.handleFollow(event));
         });
+        console.log("4");
         this.middlewareInstance.subscribeToChannelSubscriptionEvents(targetUserId, (event) => {
             console.log(`Sub event: ${targetUserId} - number of websockets: ${this.listeners.length}`);
             this.listeners.forEach((eventHandler) => eventHandler.handleSub(event));
         })
+        console.log("5");
         this.middlewareInstance.subscribeToChannelSubscriptionGiftEvents(targetUserId, (event) => {
             console.log(`Gift sub event: ${targetUserId} - number of websockets: ${this.listeners.length}`);
             this.listeners.forEach((eventHandler) => eventHandler.handleGiftSub(event));
-        })
-        this.middlewareInstance.subscribeToExtensionBitsTransactionCreateEvents(targetUserId, (event) => {
-            console.log(`Cheer Extension event: ${targetUserId} - number of websockets: ${this.listeners.length}`);
-            this.listeners.forEach((eventHandler) => eventHandler.handleCheerExtension(event));
         })
     }
 }
